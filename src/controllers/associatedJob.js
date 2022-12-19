3821ebb2db7fb01b5985b4145c0f56eff66224a4
Devlin Smith <devsmith0@gmail.com>
2022-12-19 14:28:34 +0200

    [FEAT]: Remove random data

--- src/controllers/associatedJob.js
+++ src/controllers/associatedJob.js
@@ -8,22 +8,17 @@ import db from '@/database';
  */
 export const createAssociatedJob = async (req, res, next) => {
   try {
-    // const { id: userId } = req.user;
+    const { id: userId } = req.user;
 
     const associatedJobData = {
       ...req.body,
-      userId: 1,
-      payment1: Math.floor(Math.random() * 6000),
-      payment2: Math.floor(Math.random() * 6000),
-      payment3: Math.floor(Math.random() * 6000)
+      userId: userId
     };
 
     const associatedJob = await db.models.AssociatedJob
       .create(associatedJobData, {
-        fields: ['userId', 'description', 'payment1', 'payment2', 'payment3'],
+        fields: ['userId', 'description'],
       });
-
-    associatedJob.dataValues.paymentAverage = (associatedJob.dataValues.payment1 + associatedJob.dataValues.payment2 + associatedJob.dataValues.payment3) / 3
 
     return res.status(201).json(associatedJob);
   } catch (err) {
@@ -51,9 +46,6 @@ export const getAssociatedJobs = async (req, res, next) => {
         order: [['createdAt', 'DESC']],
       });
 
-    const totalPage = Math.ceil(associatedJobListResponse.count / perPage);
-    associatedJobListResponse.rows.forEach((associatedJob) => associatedJob.dataValues.paymentAverage =
-      (associatedJob.payment1 + associatedJob.payment2 + associatedJob.payment3) / 3);
     const response = {
       ...associatedJobListResponse, page, totalPage, perPage,
     };
@@ -73,7 +65,7 @@ export const getAssociatedJobById = async (req, res, next) => {
 
     const associatedJob = await db.models.AssociatedJob
       .findOne({
-        where: { id: associatedJob },
+        where: { id: associatedJobId },
         include: {
           model: db.models.User,
           attributes: ['id', 'firstName', 'lastName'],
@@ -98,7 +90,7 @@ export const deleteAssociatedJob = async (req, res, next) => {
     const { id: userId } = req.user;
     const { id: associatedJobId } = req.params;
 
-    const associatedJob = await db.models.AssociatedJob.findOne({ where: { id: associatedJob, userId } });
+    const associatedJob = await db.models.AssociatedJob.findOne({ where: { id: associatedJobId, userId } });
     if (!associatedJob) {
       return next(createError(404, 'There is no associatedJob with this id!'));
     }
